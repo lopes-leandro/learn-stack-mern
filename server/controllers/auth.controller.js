bbc3fc809c2012c3de73d07cfc5e4542c086106a
Leandro Lopes <lopesleandro.it@outlook.com>
2020-07-02 19:41:28 -0300

    implementando o esqueleto da controller de autorização/autenticação

---
+++ server/controllers/auth.controller.js
@@ -0,0 +1,6 @@
+const signin = (req, res) => {}
+const signout = (req, res) => {}
+const requireSignin = {}
+const hasAuthorization = (req, res) => {}
+
+export default { signin, signout, requireSignin, hasAuthorization }
