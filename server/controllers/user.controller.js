4eb78809c9ce19cc81d2ca55e0cbf3ffa2eb8a87
Leandro Lopes <lopesleandro.it@outlook.com>
2020-07-02 14:00:49 -0300

    implementado o esqueleto básico do CRUD da API do usuário

---
+++ server/controllers/user.controller.js
@@ -0,0 +1,8 @@
+const create = (req, res, next) => {}
+const list = (req, res) => {}
+const userById = (req, res, next, id) => {}
+const read = (req, res) => {}
+const update = (req, res) => {}
+const remove = (req, res, next) => {}
+
+export default { create, list, userById, read, update, remove }
