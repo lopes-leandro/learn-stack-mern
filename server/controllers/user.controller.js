16cd78f53aec34bedf1fe0addc77bb89ea226d44
Leandro Lopes <lopesleandro.it@outlook.com>
2020-07-02 15:10:51 -0300

    implementado a função create para a controller user

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -1,4 +1,20 @@
-const create = (req, res, next) => {}
+import User from './../models/user.model';
+import errorHandler from './../helpers/dbErrorHandler';
+import lodash from 'lodash';
+
+const create = async (req, res, next) => {
+    const user = new User(req.body);
+    try {
+        await user.save();
+        return res.status(200).json({
+            message: 'Cadastrado realizado com sucesso!'
+        });
+    } catch (err) {
+        return res.status(400).json({
+            message: errorHandler.getErrorMessage(err)
+        });
+    }
+}
 const list = (req, res) => {}
 const userById = (req, res, next, id) => {}
 const read = (req, res) => {}
