8ea7f0415bd7041646bf2fa6e2e0d45d42e54fa6
Leandro Lopes <lopesleandro.it@outlook.com>
2020-07-02 20:58:04 -0300

    implementado a função signin que verifica os dados de usuário e senha informados e gera um token

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -1,4 +1,30 @@
-const signin = (req, res) => {}
+import User from './../models/user.model';
+import jwt from 'jsonwebtoken';
+import config from './../../config/config';
+
+const signin = async (req, res) => {
+    try {
+        let user = await User.findOne({"email": req.body.email});
+        if (!user) {
+            return res.status(401).json({error: 'Usuário não encontrado.'});
+        }
+        if (!user.authenticate(req.body.password)) {
+            return res.status(401).send({error: 'Email e password não coincidem.'});
+        }
+        const token = jwt.sign({_id: user._id}, config.jwtSecret);
+        res.cookie('t', token, { expires: new Date() + 9999});
+        return res.json({
+            token,
+            user: {
+                _id: user._id,
+                name: user.name,
+                email: user.email
+            }
+        });
+    } catch (err) {
+        return res.status(401).json({error: 'Não foi possível entrar.'});
+    }
+}
 const signout = (req, res) => {}
 const requireSignin = {}
 const hasAuthorization = (req, res) => {}
