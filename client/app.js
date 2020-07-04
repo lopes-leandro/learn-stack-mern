4ceb512bc59286060ad15dfff630c71e9dea20bf
Leandro Lopes <lopesleandro.it@outlook.com>
2020-07-04 13:05:56 -0300

    atualizado o componente App para disponibilizar acesso as rotas especificadas no componente MainRouter

--- client/app.js
+++ client/app.js
@@ -2,11 +2,13 @@ import React from 'react';
 import {BrowserRouter} from 'react-router-dom';
 import {ThemeProvider} from '@material-ui/styles';
 import {hot} from 'react-hot-loader';
+import {MainRouter} from './MainRouter';
 
 const App = () => {
     return(
         <BrowserRouter>
             <ThemeProvider>
+                <MainRouter/>
             </ThemeProvider>
         </BrowserRouter>
     );
