3c0601c36464b948fd7c735c567f66a11615f314
Leandro Lopes <lopesleandro.it@outlook.com>
2020-07-04 12:49:03 -0300

    atualizando o componente principal App para exportação rápida

--- client/app.js
+++ client/app.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import {BrowserRouter} from 'react-router-dom';
 import {ThemeProvider} from '@material-ui/styles';
+import {hot} from 'react-hot-loader';
 
 const App = () => {
     return(
@@ -11,4 +12,4 @@ const App = () => {
     );
 }
 
-export default App;
+export default hot(module)(App);
