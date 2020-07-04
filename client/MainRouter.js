ee5814a9601ba73e8c0d1dca2afe7e119b7c78d5
Leandro Lopes <lopesleandro.it@outlook.com>
2020-07-04 13:01:08 -0300

    definindo o arquivo de rotas que ajudará na renderização dos componentes personalizados

---
+++ client/MainRouter.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Route, Switch} from 'react-router-dom';
+
+const MainRouter = () => {
+    return(<div>
+        <Switch>
+            <Route exact path="/"></Route>
+        </Switch>
+    </div>)
+}
+
+export default MainRouter;
