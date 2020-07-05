85e368ae875aab204be8b6dc8feda7299a7580e9
Leandro Lopes <lopesleandro.it@outlook.com>
2020-07-05 10:37:36 -0300

    implementado o método de consumo da API Remove User do back-end que deleta um usuário pesquisado por Id

--- client/user/api-user.js
+++ client/user/api-user.js
@@ -63,3 +63,19 @@ const update = async(params, credentials, user) => {
     }
 }
 
+const remove = async(params, credentials) => {
+    try {
+        let response = await fetch(`/api/v1/users/${params.userId}`, {
+            method: 'DELETE',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json',
+                'Autorization': 'Bearer ' + credentials.t
+            }
+        });
+        return await response.json();
+    } catch (err) {
+        console.log(err);        
+    }
+}
+
