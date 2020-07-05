16b90c2072bfed89be4602c7e5e12b194e9a09b3
Leandro Lopes <lopesleandro.it@outlook.com>
2020-07-05 14:46:37 -0300

    implementado o método para recuperar o token armazenado

--- client/auth/auth-helper.js
+++ client/auth/auth-helper.js
@@ -4,3 +4,14 @@ authenticate(jwt, cb) {
     }
     cb();
 }
+
+isAuthenticated() {
+    if (typeof window == "undefined") {
+        return false;
+    }
+    if (sessionStorage.getItem('jwt')) {
+        return JSON.parse(sessionStorage.getItem('jwt'));
+    } else {
+        return false;
+    }
+}
